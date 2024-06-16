4d4b6f0e019bf9a741b4530a80a135d202e2cfaa
Kamil CALISKAN <37404388+KamilCaliskan@users.noreply.github.com>
2024-06-16 16:05:50 +0200

    Update and rename JavaScript_exercise.js to Training/JavaScript_exercise.js

--- Training/JavaScript_exercise.js
+++ Training/JavaScript_exercise.js
@@ -1,12 +1,12 @@
 //In a small town the population is p0 = 1000 at the beginning of a year. 
-The population regularly increases by 2 percent per year and moreover 50 new inhabitants 
-per year come to live in the town. How many years does the town need to see 
-its population greater or equal to p = 1200 inhabitants?//
+//The population regularly increases by 2 percent per year and moreover 50 new inhabitants 
+//per year come to live in the town. How many years does the town need to see 
+//its population greater or equal to p = 1200 inhabitants?
 
 
 
 
-    // Define a function called nbYear with parameters p0 (initial population),
+ // Define a function called nbYear with parameters p0 (initial population),
 // percent (percentage increase per year), aug (additional inhabitants arriving per year),
 // and p (target population).
 function nbYear(p0, percent, aug, p) {
