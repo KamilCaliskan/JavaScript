3b945476b804d3f17bf4707dd1269a6ee6001026
Kamil CALISKAN <37404388+KamilCaliskan@users.noreply.github.com>
2024-06-21 18:24:58 +0200

    Update test.js

--- Training/HelloWorld/test.js
+++ Training/HelloWorld/test.js
@@ -3,8 +3,23 @@ const { assert } = require('chai');
 const helloWorld = require('./helloWorld');
 
 describe("Tests", () => {
-  it("test", () => {
+  it("Test for mission 1: should create a function helloWorld", () => {
     assert.isFunction(helloWorld, "function helloWorld is not defined");
+  });
+
+  it("Test for mission 2: should define a variable str and set value", () => {
+    const funcStr = helloWorld.toString();
+    assert.match(funcStr, /(var|let|const) +str/, "variable str is not defined");
+  });
+
+  it("Test for mission 3: should use method console.log() to print Hello World!", () => {
+    let loggedMessage = "";
+    const originalLog = console.log;
+    console.log = (message) => { loggedMessage = message; };
+    
     helloWorld();
+    
+    console.log = originalLog;
+    assert.strictEqual(loggedMessage, "Hello World!", 'You did not print "Hello World!" to the screen');
   });
 });
