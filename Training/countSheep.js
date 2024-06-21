25ed3f583744de232a7428a2cec73efc595c1e9c
Kamil CALISKAN <37404388+KamilCaliskan@users.noreply.github.com>
2024-06-21 14:48:52 +0200

    Create countSheep.js

---
+++ Training/countSheep.js
@@ -0,0 +1,22 @@
+function countSheeps(sheep) {
+  let count = 0;
+  for (let i = 0; i < sheep.length; i++) {
+    if (sheep[i] === true) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// Example usage:
+const sheepArray = [
+  true,  true,  true,  false,
+  true,  true,  true,  true,
+  true,  false, true,  false,
+  true,  false, false, true,
+  true,  true,  true,  true,
+  false, false, true,  true
+];
+
+// Output: 17
+console.log(countSheeps(sheepArray));
