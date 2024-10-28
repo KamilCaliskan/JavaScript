2b326edf1f9f9f1c41dddde0e45fc1c77e02a055
Kamil ÇALIŞKAN <37404388+KamilCaliskan@users.noreply.github.com>
2024-10-28 12:02:45 +0100

    Update FindDigit.js

--- Training/FindDigit.js
+++ Training/FindDigit.js
@@ -75,7 +75,22 @@ function solveExpression(exp) {
     return -1; // If no valid digit is found
 }
 
-// Example usage
-console.log(solveExpression("1+1=?"));         // Output: 0
-console.log(solveExpression("123*45?=5?088")); // Output: 0
-console.log(solveExpression("-5?*-1=5?"));     // Output: -1
+// Example Test Cases
+const { assert } = require('chai');
+
+describe('Example tests', () => {
+    const testCases = [
+        ['1+1=?', 2],
+        ['123*45?=5?088', 6],
+        ['-5?*-1=5?', 0],
+        ['19--45=5?', -1],
+        ['??*??=302?', 5],
+        ['?*11=??', 2],
+        ['??*1=??', 2],
+        ['??+??=??', -1]
+    ];
+    for (const [exp, expected] of testCases)
+        it(`exp = ${JSON.stringify(exp)}`, () => {
+            assert.strictEqual(solveExpression(exp), expected);
+        });
+});
